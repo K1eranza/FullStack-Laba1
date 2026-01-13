9f2dc10b1d299931448b4cf47805163a0e454a80
pomarulit007-cyber <poma.rulit007@gmail.com>
2026-01-13 13:37:30 +0300

    Terminal file operations

---
+++ 1laba2semesterBerZub/backend/program.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        
+        Console.WriteLine("Меня зовут Березкин Р.А.");
+        
+        Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+    }
+}
